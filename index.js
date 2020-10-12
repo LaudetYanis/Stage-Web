bb94458bf6bdb0c21858a3a895a113c7cf6bbfab
LaudetYanis <58517047+LaudetYanis@users.noreply.github.com>
2020-10-12 13:11:20 +0200

    Base site Sopymep

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 
 
-
-var express = require('express');
+const path = require("path");
+const express = require('express');
 var app = express();
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.sendFile( __dirname + "/src/index.html")
 });
 
-app.listen(80) 
+
+
+
+app.get('/css/:name', function (req, res, next) {
+  var options = {
+    root: path.join(__dirname, 'src/css'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true,
+      'content-type': 'text/css'
+    }
+  }
+
+  var fileName = req.params.name
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
+})
+
+app.get('/js/:name', function (req, res, next) {
+  var options = {
+    root: path.join(__dirname, 'src/js'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true,
+      'content-type': 'application/javascript'
+    }
+  }
+
+  var fileName = req.params.name
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
+})
+
+app.get('*', function(req, res) {
+	res.redirect('/')
+});
+
+app.listen(80)

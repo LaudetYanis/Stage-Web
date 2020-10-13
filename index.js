5641aee6dcac3beb276495188347ba60147f5955
LaudetYanis <58517047+LaudetYanis@users.noreply.github.com>
2020-10-13 07:24:12 +0200

    suite

--- index.js
+++ index.js
@@ -10,49 +10,52 @@ app.get('/', function(req, res) {
   res.sendFile( __dirname + "/src/index.html")
 });
 
+app.get('/robot.txt', function(req, res) {
+  res.sendFile( __dirname + "/src/robot.txt")
+});
+
+app.get('/favicon.ico', function(req, res) {
+  res.sendFile( __dirname + "/src/favicon.ico")
+});
 
 
 
 app.get('/css/:name', function (req, res, next) {
-  var options = {
-    root: path.join(__dirname, 'src/css'),
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true,
-      'content-type': 'text/css'
-    }
-  }
+	var options = {
+	    root: path.join(__dirname, 'src/css'),
+	    dotfiles: 'deny',
+	    headers: {
+	    	'x-timestamp': Date.now(),
+	    	'x-sent': true,
+	    	'content-type': 'text/css'
+	    }
+	}
 
-  var fileName = req.params.name
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err)
-    } else {
-      console.log('Sent:', fileName)
-    }
-  })
+	var fileName = req.params.name
+  	res.sendFile(fileName, options, function (err) {
+    	if (err) {
+      	next(err)
+  		}
+  	})
 })
 
 app.get('/js/:name', function (req, res, next) {
-  var options = {
-    root: path.join(__dirname, 'src/js'),
-    dotfiles: 'deny',
-    headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true,
-      'content-type': 'application/javascript'
-    }
-  }
+	var options = {
+	    root: path.join(__dirname, 'src/js'),
+	    dotfiles: 'deny',
+	    headers: {
+	    	'x-timestamp': Date.now(),
+	    	'x-sent': true,
+	    	'content-type': 'application/javascript'
+	    }
+	}
 
-  var fileName = req.params.name
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      next(err)
-    } else {
-      console.log('Sent:', fileName)
-    }
-  })
+	var fileName = req.params.name
+  	res.sendFile(fileName, options, function (err) {
+    	if (err) {
+      	next(err)
+  		}
+  	})
 })
 
 app.get('*', function(req, res) {

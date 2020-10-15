ce56bd2a3e65b41dcaf10c3480a37a2ba9ba48ef
CampLSNIR <camp.leonardo@lyceeppr.fr>
2020-10-15 15:43:56 +0200

    vue js ajout link entre page + footer

--- index.js
+++ index.js
@@ -6,81 +6,85 @@ var app = express();
 const auth = require('http-auth');
 
 app.get('/', function(req, res) {
-  	res.sendFile( __dirname + "/src/index.html")
+		res.sendFile( __dirname + "/src/index.html")
+});
+
+app.get('/sopymep*', function(req, res) {
+		res.sendFile( __dirname + "/src/index.html")
 });
 
 app.get('/robots.txt', function(req, res) {
-  	res.set ('Content-Type', 'text/plain')
-  	res.sendFile( __dirname + "/src/robot.txt")
+		res.set ('Content-Type', 'text/plain')
+		res.sendFile( __dirname + "/src/robot.txt")
 });
 
 app.get('/favicon.ico', function(req, res) {
-  	res.sendFile( __dirname + "/src/favicon.ico")
+		res.sendFile( __dirname + "/src/favicon.ico")
 });
 
 console.log( auth )
 const basic = auth.basic({
-  realm: "Admin",
-  file: __dirname + "/admin.htpasswd" // user:admin114, pass:zDvzxU0RNqSaugdxzxIi
+	realm: "Admin",
+	file: __dirname + "/admin.htpasswd" // user:admin114, pass:zDvzxU0RNqSaugdxzxIi
 });
 
 
 app.get('/admin', basic.check((req, res) => {
-    res.end(`Welcome to private area - ${req.user}!`);
+		res.end(`Welcome to private area - ${req.user}!`);
 }));
 
 let domain = "http://www.example.com/"
 
 app.get('/sitemap.xml', async function(req, res, next){
-  let xml_content = [
-    '<?xml version="1.0" encoding="UTF-8"?>',
-    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-    '  <url>',
-    '    <loc>' + domain + '</loc>',
-    '    <lastmod>2020-10-13</lastmod>',
-    '  </url>',
-    '</urlset>'
-  ]
-  res.set('Content-Type', 'text/xml')
-  res.send(xml_content.join('\n'))
+	let xml_content = [
+		'<?xml version="1.0" encoding="UTF-8"?>',
+		'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+		'  <url>',
+		'    <loc>' + domain + '</loc>',
+		'    <lastmod>2020-10-13</lastmod>',
+		'  </url>',
+		'</urlset>'
+	]
+	res.set('Content-Type', 'text/xml')
+	res.send(xml_content.join('\n'))
 })
 
 app.get('/css/:name', function (req, res, next) {
 	var options = {
-	    root: path.join(__dirname, 'src/css'),
-	    dotfiles: 'deny',
-	    headers: {
-	    	'x-timestamp': Date.now(),
-	    	'x-sent': true,
-	    	'content-type': 'text/css'
-	    }
+			root: path.join(__dirname, 'src/css'),
+			dotfiles: 'deny',
+			headers: {
+				'x-timestamp': Date.now(),
+				'x-sent': true,
+				'content-type': 'text/css'
+			}
 	}
 
 	var fileName = req.params.name
-  	res.sendFile(fileName, options, function (err) {
-    	if (err) {
-      	next(err)
-  		}
-  	})
+		res.sendFile(fileName, options, function (err) {
+			if (err) {
+				next(err)
+			}
+		})
 })
 
 app.get('/js/:name', function (req, res, next) {
 	var options = {
-	    root: path.join(__dirname, 'src/js'),
-	    dotfiles: 'deny',
-	    headers: {
-	    	'x-timestamp': Date.now(),
-	    	'x-sent': true,
-	    	'content-type': 'application/javascript'
-	    }
+			root: path.join(__dirname, 'src/js'),
+			dotfiles: 'deny',
+			headers: {
+				'x-timestamp': Date.now(),
+				'x-sent': true,
+				'content-type': 'application/javascript'
+			}
 	}
 
 	var fileName = req.params.name
-  	res.sendFile(fileName, options, function (err) {
-    	if (err) {
-      	next(err)
-  		}
-  	})
+		res.sendFile(fileName, options, function (err) {
+			if (err) {
+				next(err)
+			}
+		})
 })
 
 app.get('*', function(req, res) {
--- src/js/script.js
+++ src/js/script.js
@@ -90,6 +90,8 @@ function HideNavbar(){
 function GrabNews(){
 
 	const ele = document.getElementById('card-list');
+
+	if( !ele ) return;
 	ele.style.cursor = 'grab';
 	
 	let pos = { top: 0, left: 0, x: 0, y: 0 };
@@ -138,7 +140,7 @@ function GrabNews(){
 Vue.component('navbar', {
   template: `
 	<div class="container">
-		<div class="navbar" id= "nav">
+		<div class="navbar shadow-lg" id= "nav">
 			<a href="#Home">HALOGMA</a>
 			<div class="dropdown">
 				<a class="drop active" href="javascript:void(0);">SOPYMEP</a>
@@ -164,7 +166,7 @@ Vue.component('navbar', {
 				</div>
 			</div>
 			<router-link to="/contact" id="Contact">Contact</router-link>
-			<a href="javascript:void(0);" class="icon" onclick="displayMenu()"><i class="fa fa-bars"></i></a>
+			<a href="javascript:void(0);" class="icon" aria-label="menu" onclick="displayMenu()"><i class="fa fa-bars"></i></a>
 		</div>
 		<div class="navbargap"></div>
 	</div>`
@@ -181,7 +183,9 @@ Vue.component('news-card', {
 				<p class="date">{{date}}</p>
 				<h2 class="card-title">{{title}}</h2>
 				<p class="body-content">{{desc}}</p>
-				<button class="button button-primary">En savoir plus</button>
+				<router-link class="button button-primary" v-bind:to="'/sopymep/news/'+ this.id"id="Contact">
+					<button class="button button-primary">En savoir plus</button>
+				</router-link>
 			</div>
 		</article>
 	`
@@ -226,43 +230,61 @@ const Bar = { template: '<div>bar</div>' }
 
 const sopymep = { template: `
 	<div>
-		<navbar></navbar>
-		<transition name="fade">
-			<div>
-				<div class="container presentation" >
-					<h1><div class="logo">SOPYMEP</div> du groupe HALOGMA</h1>
-				</div>
-				<hr>
+		<div>
+			<div class="container presentation" >
+				<h1><div class="logo">SOPYMEP</div> du groupe HALOGMA</h1>
 			</div>
-		</transition>
+			<hr>
+		</div>
 	</div>
 ` }
 
 const sopymepnews = { 
-	props: [ 'posts' ], 
+	props: [ 'posts' , 'salut' , 'btnClickHandler' ], 
 	template: `
 	<div>
-		<navbar></navbar>
-		<transition name="fade">
-			<div>
-				<div class="container title">
-					<div>News</div>
+		<div>
+			<div class="container title">
+				<div>News {{btnClickHandler}}</div>
+			</div>
+			<div class="card-list container info" id="card-list">
+				<news-card v-for="post in posts" v-bind:key="post.id" v-bind:title="post.title" v-bind:desc="post.desc" v-bind:date="post.date" v-bind:id="post.id"></news-card>
+			</div>
+		</div>
+	</div>
+` }
+
+const sopymeparticle = { 
+	props: [ 'posts' , 'article' , 'id' ] , 
+	template: `
+	<div>
+		<div class="wrapper">
+			<div class="news-container">
+				<div class="news__header">
+					<img class="header__thumbnail" src="https://source.unsplash.com/1920x1080/" alt="" />
 				</div>
-				<div class="card-list container info" id="card-list">
-					<news-card v-for="post in posts" v-bind:key="post.id" v-bind:title="post.title"></news-card>
+				
+				<div class="news__body">
+					<h1 class="body__heading">{{article.title}}</h1>
+					
+					<small class="body__meta">{{article.date}}</small>
+					
+					<p class="body__text" v-for="v in (article.content.split('\\n'))">{{v}}</p>
 				</div>
 			</div>
-		</transition>
+		</div>
+		<div>
+			<div class="card-list container info" id="card-list">
+				<news-card v-for="post in posts" v-bind:key="post.id" v-bind:title="post.title" v-bind:desc="post.desc" v-bind:date="post.date" v-bind:id="post.id"></news-card>
+			</div>
+		</div>
 	</div>
 ` }
 
 const contact = { 
 	template: `
 	<div>
-		<navbar></navbar>
-		<transition name="fade">
-			<halogma-form></halogma-form>
-		</transition>
+		<halogma-form></halogma-form>
 	</div>
 ` }
 
@@ -281,17 +303,56 @@ const routes = [
 			  { id: 1, title: 'Mon initiation avec Vue' , date : '25 mars 2020' , desc : 'desc' },
 			  { id: 2, title: 'Blogger avec Vue' , date : '25 mars 2020' , desc : 'desc' },
 			  { id: 3, title: 'Pourquoi Vue est tellement cool' , date : '25 mars 2020' , desc : 'desc' },
-			  { id: 4, title: 'vuejs yay' , date : '25 mars 2020' , desc : 'desc' }
+			  { id: 4, title: 'vuejs yay' , date : '25 mars 2020' , desc : '' }
 			],
-		}  
-		
+			salut : async function(){ console.log( this ); return "ok"; },
+			btnClickHandler: { type: Function }
+		}
 	},
 	{ 
 		path: '/contact', 
 		component: contact , 
 
 	},
-	{ path: '/news/:id', component: Bar }
+	{ 
+		path: '/sopymep/news/:id', 
+		component: sopymeparticle ,
+		props: {
+			posts: [
+			  { id: 1, title: 'Mon initiation avec Vue' , date : '25 mars 2020' , desc : 'desc' },
+			  { id: 2, title: 'Blogger avec Vue' , date : '25 mars 2020' , desc : 'desc' },
+			  { id: 3, title: 'Pourquoi Vue est tellement cool' , date : '25 mars 2020' , desc : 'desc' },
+			  { id: 4, title: 'vuejs yay' , date : '25 mars 2020' , desc : 'Ob haec et huius modi multa, quae cernebantur in paucis, omnibus timeri sunt coepta. et ne tot malis dissimulatis paulatimque serpentibus acervi crescerent aerumnarum, nobilitatis decreto legati mittuntur: Praetextatus ex urbi praefecto et ex vicario Venust' }
+			],
+			article: {
+				date: "25 mars 2020",
+				desc: "salut desc",
+				title: "salut title",
+				content : `Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam ratus tamquam obtrectatorem audacem excarnificari praecepit, qui ita evisceratus ut cruciatibus membra deessent, inplorans caelo iustitiam, torvum renidens fundato pectore mansit inmobilis nec se incusare nec quemquam alium passus et tandem nec confessus nec confutatus cum abiecto consorte poenali est morte multatus. et ducebatur intrepidus temporum iniquitati insultans, imitatus Zenonem illum veterem Stoicum qui ut mentiretur quaedam laceratus diutius, avulsam sedibus linguam suam cum cruento sputamine in oculos interrogantis Cyprii regis inpegit.
+Denique Antiochensis ordinis vertices sub uno elogio iussit occidi ideo efferatus, quod ei celebrari vilitatem intempestivam urgenti, cum inpenderet inopia, gravius rationabili responderunt; et perissent ad unum ni comes orientis tunc Honoratus fixa constantia restitisset.
+Raptim igitur properantes ut motus sui rumores celeritate nimia praevenirent, vigore corporum ac levitate confisi per flexuosas semitas ad summitates collium tardius evadebant. et cum superatis difficultatibus arduis ad supercilia venissent fluvii Melanis alti et verticosi, qui pro muro tuetur accolas circumfusus, augente nocte adulta terrorem quievere paulisper lucem opperientes. arbitrabantur enim nullo inpediente transgressi inopino adcursu adposita quaeque vastare, sed in cassum labores pertulere gravissimos.
+Post emensos insuperabilis expeditionis eventus languentibus partium animis, quas periculorum varietas fregerat et laborum, nondum tubarum cessante clangore vel milite locato per stationes hibernas, fortunae saevientis procellae tempestates alias rebus infudere communibus per multa illa et dira facinora Caesaris Galli, qui ex squalore imo miseriarum in aetatis adultae primitiis ad principale culmen insperato saltu provectus ultra terminos potestatis delatae procurrens asperitate nimia cuncta foedabat. propinquitate enim regiae stirpis gentilitateque etiam tum Constantini nominis efferebatur in fastus, si plus valuisset, ausurus hostilia in auctorem suae felicitatis, ut videbatur.
+Hae duae provinciae bello quondam piratico catervis mixtae praedonum a Servilio pro consule missae sub iugum factae sunt vectigales. et hae quidem regiones velut in prominenti terrarum lingua positae ob orbe eoo monte Amano disparantur.
+Qui cum venisset ob haec festinatis itineribus Antiochiam, praestrictis palatii ianuis, contempto Caesare, quem videri decuerat, ad praetorium cum pompa sollemni perrexit morbosque diu causatus nec regiam introiit nec processit in publicum, sed abditus multa in eius moliebatur exitium addens quaedam relationibus supervacua, quas subinde dimittebat ad principem.
+Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit, putet esse leviorem, dum modo, cuius exemplo se consolatur, eius exitum expectet, praesertim cum in Albucio nec Pisonis libidines nec audacia Gabini fuerit ac tamen hac una plaga conciderit, ignominia senatus.
+Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.
+Et licet quocumque oculos flexeris feminas adfatim multas spectare cirratas, quibus, si nupsissent, per aetatem ter iam nixus poterat suppetere liberorum, ad usque taedium pedibus pavimenta tergentes iactari volucriter gyris, dum exprimunt innumera simulacra, quae finxere fabulae theatrales.
+Adolescebat autem obstinatum propositum erga haec et similia multa scrutanda, stimulos admovente regina, quae abrupte mariti fortunas trudebat in exitium praeceps, cum eum potius lenitate feminea ad veritatis humanitatisque viam reducere utilia suadendo deberet, ut in Gordianorum actibus factitasse Maximini truculenti illius imperatoris rettulimus coniugem.
+Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.
+Quapropter a natura mihi videtur potius quam ab indigentia orta amicitia, applicatione magis animi cum quodam sensu amandi quam cogitatione quantum illa res utilitatis esset habitura. Quod quidem quale sit, etiam in bestiis quibusdam animadverti potest, quae ex se natos ita amant ad quoddam tempus et ab eis ita amantur ut facile earum sensus appareat. Quod in homine multo est evidentius, primum ex ea caritate quae est inter natos et parentes, quae dirimi nisi detestabili scelere non potest; deinde cum similis sensus exstitit amoris, si aliquem nacti sumus cuius cum moribus et natura congruamus, quod in eo quasi lumen aliquod probitatis et virtutis perspicere videamur.
+Superatis Tauri montis verticibus qui ad solis ortum sublimius attolluntur, Cilicia spatiis porrigitur late distentis dives bonis omnibus terra, eiusque lateri dextro adnexa Isauria, pari sorte uberi palmite viget et frugibus minutis, quam mediam navigabile flumen Calycadnus interscindit.
+Rogatus ad ultimum admissusque in consistorium ambage nulla praegressa inconsiderate et leviter proficiscere inquit ut praeceptum est, Caesar sciens quod si cessaveris, et tuas et palatii tui auferri iubebo prope diem annonas. hocque solo contumaciter dicto subiratus abscessit nec in conspectum eius postea venit saepius arcessitus.
+Quam ob rem id primum videamus, si placet, quatenus amor in amicitia progredi debeat. Numne, si Coriolanus habuit amicos, ferre contra patriam arma illi cum Coriolano debuerunt? num Vecellinum amici regnum adpetentem, num Maelium debuerunt iuvare?
+Eo adducta re per Isauriam, rege Persarum bellis finitimis inligato repellenteque a conlimitiis suis ferocissimas gentes, quae mente quadam versabili hostiliter eum saepe incessunt et in nos arma moventem aliquotiens iuvant, Nohodares quidam nomine e numero optimatum, incursare Mesopotamiam quotiens copia dederit ordinatus, explorabat nostra sollicite, si repperisset usquam locum vi subita perrupturus.
+Ut enim benefici liberalesque sumus, non ut exigamus gratiam (neque enim beneficium faeneramur sed natura propensi ad liberalitatem sumus), sic amicitiam non spe mercedis adducti sed quod omnis eius fructus in ipso amore inest, expetendam putamus.
+Per hoc minui studium suum existimans Paulus, ut erat in conplicandis negotiis artifex dirus, unde ei Catenae inditum est cognomentum, vicarium ipsum eos quibus praeerat adhuc defensantem ad sortem periculorum communium traxit. et instabat ut eum quoque cum tribunis et aliis pluribus ad comitatum imperatoris vinctum perduceret: quo percitus ille exitio urgente abrupto ferro eundem adoritur Paulum. et quia languente dextera, letaliter ferire non potuit, iam districtum mucronem in proprium latus inpegit. hocque deformi genere mortis excessit e vita iustissimus rector ausus miserabiles casus levare multorum.
+Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.
+Omitto iuris dictionem in libera civitate contra leges senatusque consulta; caedes relinquo; libidines praetereo, quarum acerbissimum extat indicium et ad insignem memoriam turpitudinis et paene ad iustum odium imperii nostri, quod constat nobilissimas virgines se in puteos abiecisse et morte voluntaria necessariam turpitudinem depulisse. Nec haec idcirco omitto, quod non gravissima sint, sed quia nunc sine teste dico.
+`
+			},
+		}
+
+	}
 ]
 
 const router = new VueRouter({
@@ -304,12 +365,7 @@ let vm = new Vue({
   router : router,
   el: '#app',
   data: {
-	posts: [
-	  { id: 1, title: 'Mon initiation avec Vue' , date : '25 mars 2020' , desc : 'desc' },
-	  { id: 2, title: 'Blogger avec Vue' , date : '25 mars 2020' , desc : 'desc' },
-	  { id: 3, title: 'Pourquoi Vue est tellement cool' , date : '25 mars 2020' , desc : 'desc' },
-	  { id: 4, title: 'vuejs yay' , date : '25 mars 2020' , desc : 'desc' }
-	]
+	
   },
   computed: {
   }
@@ -317,8 +373,20 @@ let vm = new Vue({
 
 router.beforeEach((to, from, next) => {
 	HideNavbar()
+
+	
 	next()
+
+	Vue.nextTick(function () {
+		
+	});
+
+	setTimeout(function(){
+		GrabNews()
+	}, 100);
+
+	console.log( to, from )
 })
 
 
-//GrabNews()
+GrabNews()
